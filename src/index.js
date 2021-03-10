a6db49a549622877ecfd68ba2be873d5144ec023
EvvTim <evgeniytim94@gmail.com>
2021-03-10 15:43:15 +0100

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+
+    if(array === undefined || array.length === 0 ) {
+        return 0
+    } else {
+        array.sort((a, b) => a-b)
+        return array[0]
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0 ) {
+        return 0
+    } else {
+        array.sort((a, b) => a-b).reverse()
+        return array[0]
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0 ) {
+        return 0
+    } else {
+        return array.reduce((a,b) => a + b)/array.length
+    }
 }
